fc80acee5088656a449a791dba56c87d8c46425e
Hamed Ghazali <hamedg20@yahoo.com>
2022-03-30 03:58:03 -0700

    top log lines report

--- gatther-information.sh
+++ gatther-information.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+line='------------------------'
 
 echo "Starting at: $(date)"
-echo
+echo $line
 
 echo "UPTIME"
 uptime
-echo
+echo $line
 
 echo "FREE"
 vm_stat
-echo
+echo $line
 
 echo "WHO"
 who
-echo
+echo $line
 
 echo "Finishing at: $(date)"
